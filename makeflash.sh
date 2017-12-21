a899c0955d2ed4dd2338289f24a9660c0e6b41ff
Cherry (Zededa) <33273628+cherry-zededa-com@users.noreply.github.com>
2017-12-21 13:44:04 +0000

    Make scripts amd64 independant

--- makeflash.sh
+++ makeflash.sh
@@ -3,7 +3,8 @@
 #
 #      ./makeflash.sh [-C] <output.img>
 #
-MKFLASH_TAG="$(linuxkit pkg show-tag pkg/mkflash)-amd64"
+ARCH=$(uname -m|sed s/aarch64/arm64/|s/x86_64/amd64/)
+MKFLASH_TAG="$(linuxkit pkg show-tag pkg/mkflash)-$ARCH"
 
 if [ "$1" = "-C" ]; then
     IMAGE=$2
--- makerootfs.sh
+++ makerootfs.sh
@@ -3,6 +3,7 @@
 #
 #     ./makerootfs.sh <image.yml> <output.img>
 #
-MKROOTFS_TAG="$(linuxkit pkg show-tag pkg/mkrootfs-ext4)-amd64"
+ARCH=$(uname -m|sed s/aarch64/arm64/|sed s/x86_64/amd64/)
+MKROOTFS_TAG=$(linuxkit pkg show-tag pkg/mkrootfs-ext4)-$ARCH
 
 linuxkit build -o - $1 | docker run -v /dev:/dev --privileged -i ${MKROOTFS_TAG} > $2
